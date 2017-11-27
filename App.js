83daafba8f654585dd25f454799b424146e1d944
MuslumGuzel <guzel.muslum8@gmail.com>
2017-11-28 02:19:53 +0300

    First commit

---
+++ App.js
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  ListView,
+  Image
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class Movies extends Component<{}> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    return fetch('http://192.168.1.20:3000/movies')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          isLoading: false,
+          dataSource: ds.cloneWithRows(responseJson),
+        }, function() {
+          // do something with new state
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    return (
+      <View style={{flex: 1, paddingTop: 20}}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <View>
+            <Image source={{ uri: rowData.cover_image}} />
+            <Text>{rowData.name} {rowData.description}</Text>
+            </View>}
+        />
+      </View>
+    );
+  }
+}
---
+++ Readme.md
@@ -0,0 +1,15 @@
+Start emulator
+
+Emulator path
+C:\Users\mus\AppData\Local\Android\Sdk\emulator
+
+toList Emulator
+./emulator -avd -list-avds
+
+Start in ps
+./emulator -avd [avd_name]
+
+-----------------------------------------------------
+
+Start react-native app
+react-native run-android
